ed190524ef1b2247ebfb6edb8808c6f9cfb153c5
Michani M. De La Calleja E <dev.michani@protonmail.co>
2019-04-09 23:15:22 -0500

    fixed data dictionary.cc

--- include/data_dictionary.h
+++ include/data_dictionary.h
@@ -16,18 +16,18 @@ namespace dictionary {
     class DataDictionary {
     
         public:
-             
+            // Constructor & destructors
             DataDictionary();
             DataDictionary(const std::string& name);
             ~DataDictionary();
-
+            // Setters
             void SetId(int id);
             void SetName(const std::string& name);
             void SetDir(const std::string& dir); 
             void SetPath(const std::string& path); 
             void SetExt(const std::string& ext);
             void SetFileHeader(long int file_header);
-
+            // Getters
             int GetId();
             std::string GetName();
             std::string GetDir();
@@ -36,26 +36,27 @@ namespace dictionary {
             long int GetFileHeader();
             long int GetFileSize();
             
+            // Methods of File
             void CreateFile();
             void UpdateHeader();
-            
-            void UpdateAddress(long int position, long int new_address);            
-
-            void UpdateEntity(Entity *entity);
-            void AddEntity(Entity entity);
-
+            void UpdateAddress(long int position, long int new_address);
             void UpdateName(long int position, std::string new_name);
-            std::list<Entity> ReadListEntities();
-            void AddAttribute(Attribute attribute);
 
-            /*
-            long int UpdateEntity(long int position, Entity entity);
-            long int UpdateAttribute(long int position, Attribute attribute);
-            long int RemoveEntity(long int position, Entity entity);
-            long int RemoveAttribute(long int position, Attribute attribute);
-            long int ReadEntity(long int position, Entity entity);
-            long int ReadAttribute(long int position, Attribute attribute);
-            */
+            void UpdateChar(long int position, char new_char);
+            void UpdateInt(long int position, int new_int);
+
+            // Methods of entities
+            void AddEntity(Entity entity);
+            void UpdateEntity(std::list<Entity> list_entities, Entity *entity);
+            void RemoveEntity(std::list<Entity> list_entities, std::string remove_entity);
+            Entity SearchEntity(std::list<Entity> list_entities, std::string new_entity);            
+            std::list<Entity> ReadListEntities();
+            
+            void AddAttribute(Attribute attribute);
+            void UpdateAttribute(std::list<Attribute> list_attributes, Attribute attribute);
+            std::list<Attribute> ReadListAttributes(Entity entity);
+            void RemoveAttribute(Entity current_entity, std::list<Attribute> list_attributes, std::string remove_attribute);
+            Attribute SearchAttribute(std::list<Attribute> list_attributes, std::string name_attribute);
 
         private:
             // file metadata
@@ -64,14 +65,8 @@ namespace dictionary {
             std::string dir_;
             std::string path_;
             std::string ext_;
-
             long int file_header_;
             long int file_size_;
-
-            std::list<Entity> list_entities_;
-            std::list<Attribute> list_attributes_;
-
-
     };
 
 
